10f1c547eda6c380ecce1e7279036d9a8f686fad
ALBINA <4aroika@gmail.com>
2024-02-21 22:54:35 +0100

    task4

---
+++ lesson 2/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите натуральное число N");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+
+while (number > 0)
+{
+    int digit = number % 10; // Получаем последнюю цифру числа
+    Console.Write(digit);
+
+    number /= 10; // Убираем последнюю цифру
+    if (number > 0)
+    {
+        Console.Write(", ");
+    }
+}
