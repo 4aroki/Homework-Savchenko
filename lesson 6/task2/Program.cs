606bfccde41b150d88cdba31542b8ed6dcd940dd
ALBINA <4aroika@gmail.com>
2024-02-25 14:43:57 +0100

    task2

--- lesson 6/task2/Program.cs
+++ lesson 6/task2/Program.cs
@@ -1,32 +1,24 @@
 ﻿//Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
 
-    static void Main()
+
+string ConvertUpperToLower(string input)
+{
+
+    char[] chars = input.ToCharArray();
+    for (int i = 0; i < chars.Length; i++)
     {
-        // Задаем строку с латинскими буквами в обоих регистрах
-        string inputString = "HeLLoWorLD";
 
-        // Вызываем метод для замены всех заглавных букв на строчные
-        string resultString = ConvertToUpperToLower(inputString);
-
-        // Выводим результат
-        Console.WriteLine(resultString);
-    }
-
-    static string ConvertToUpperToLower(string input)
-    {
-        // Проходим по каждому символу в строке
-        char[] chars = input.ToCharArray();
-        for (int i = 0; i < chars.Length; i++)
+        if (char.IsUpper(chars[i]))
         {
-            // Проверяем, является ли символ заглавной буквой
-            if (char.IsUpper(chars[i]))
-            {
-                // Заменяем заглавную букву на строчную
-                chars[i] = char.ToLower(chars[i]);
-            }
-        }
 
-        // Формируем строку из измененных символов
-        return new string(chars);
+            chars[i] = char.ToLower(chars[i]);
+        }
     }
 
+
+    return new string(chars);
+}
+
+string inputString = "HeLLoWorLD";
+string resultString = ConvertUpperToLower(inputString);
+Console.WriteLine(resultString);
