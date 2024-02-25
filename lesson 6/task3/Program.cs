37c54d9b658eb5eebda28efc2a49e7719d0ba604
ALBINA <4aroika@gmail.com>
2024-02-25 14:50:40 +0100

    task3

--- lesson 6/task3/Program.cs
+++ lesson 6/task3/Program.cs
@@ -1,8 +1,21 @@
 ﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
 
-    static void Main()
+    bool IsPalindrome(string input)
     {
-        Console.WriteLine("Введите строку:");
+
+      int length = input.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (input[i] != input[length - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+      Console.WriteLine("Введите строку:");
         string inputString = Console.ReadLine();
 
         if (IsPalindrome(inputString))
@@ -13,22 +26,3 @@
         {
             Console.WriteLine("Это не палиндром.");
         }
-    }
-
-    static bool IsPalindrome(string str)
-    {
-        // Убираем пробелы и делаем все буквы строчными для удобства сравнения
-        string cleanedStr = str.Replace(" ", "").ToLower();
-
-        int length = cleanedStr.Length;
-        for (int i = 0; i < length / 2; i++)
-        {
-            if (cleanedStr[i] != cleanedStr[length - 1 - i])
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
