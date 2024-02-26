48fed3f8a5668d22a420b35b722dfe38c763dcfb
ALBINA <4aroika@gmail.com>
2024-02-26 14:13:21 +0100

    task3

---
+++ recursion/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void PrintReversed(int[] arr, int i)
+{
+
+    i = i - 1;
+    if (i < 0)
+    {
+        return;
+    }
+
+    Console.WriteLine(arr[i]);
+
+    PrintReversed(arr, i);
+}
+
+
+int[] array = { 1, 2, 3, 4, 5};
+
+PrintReversed(array, array.Length);
+
+
